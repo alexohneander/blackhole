fa2f11687f20f487c358efff925628c16ee51b6e
Alex Wellnitz <moin@wellnitz-alex.de>
2023-07-01 19:58:11 +0200

    Fix Typo

--- desktop/src-tauri/src/helpers/token.rs
+++ desktop/src-tauri/src/helpers/token.rs
@@ -1,7 +1,7 @@
 use rand::Rng;
 use uuid::Uuid;
 
-pub fn generate_troken() -> String {
+pub fn generate_token() -> String {
     let pizza_ingriedients = vec!["cheese", "tomato", "pepperoni", "mushrooms", "olives", "onions", "pineapple", "ham", "bacon", "chicken", "beef", "sausage", "peppers", "spinach", "anchovies", "jalapenos", "garlic", "artichokes", "broccoli", "basil", "feta", "goat-cheese", "ricotta", "provolone", "parmesan", "asiago", "cheddar", "mozzarella", "romano", "blue-cheese", "gorgonzola", "swiss-cheese", "cheddar-cheese"];
     
     let mut pizza = String::new();
